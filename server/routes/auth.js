a14bec9695f3ed9b78f16616bf3545a1dc6c6772
VadimLNN <svobodolubsev@inbox.ru>
2025-08-04 14:52:04 +0700

    рефактор валидации

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,9 +8,15 @@ const authService = require("../services/authService");
 
 router.post(
     "/register",
+    body("username").isLength({ min: 3 }).withMessage("Username must be at least 3 characters long"),
     body("email").isEmail().withMessage("Please enter a valid email"),
     body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
     async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            // Если есть ошибки, возвращаем 400 и список ошибок
+            return res.status(400).json({ errors: errors.array() });
+        }
         try {
             const newUser = await authService.register(req.body);
             res.status(201).json(newUser);
@@ -20,14 +26,24 @@ router.post(
     }
 );
 
-router.post("/login", async (req, res) => {
-    try {
-        const result = await authService.login(req.body);
-        res.status(200).json(result);
-    } catch (error) {
-        res.status(error.statusCode || 500).json({ error: error.message });
+router.post(
+    "/login",
+    body("username").isLength({ min: 3 }).withMessage("Username must be at least 3 characters long"),
+    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            // Если есть ошибки, возвращаем 400 и список ошибок
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            const result = await authService.login(req.body);
+            res.status(200).json(result);
+        } catch (error) {
+            res.status(error.statusCode || 500).json({ error: error.message });
+        }
     }
-});
+);
 
 router.get("/user", authMiddleware, async (req, res) => {
     try {
