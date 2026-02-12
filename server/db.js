5f94883d1342266afe2902f785c7e81e38537f79
VadimLNN <svobodolubsev@inbox.ru>
2026-02-12 20:47:38 +0700

    .

--- server/db.js
+++ server/db.js
@@ -7,16 +7,20 @@ const databaseName = isTestEnvironment ? process.env.DB_TEST_DATABASE : process.
 // console.log(`Node environment: ${process.env.NODE_ENV}`);
 // console.log(`Connecting to database: ${databaseName}`);
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: databaseName, // <-- Используем динамическое имя
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    max: 20, // Максимум соединений в пуле
-    idleTimeoutMillis: 30000, // Таймаут простоя
-    connectionTimeoutMillis: 2000, // Таймаут подключения
-});
+const pool = new Pool(
+    process.env.DATABASE_URL
+        ? {
+              connectionString: process.env.DATABASE_URL,
+              ssl: { rejectUnauthorized: false },
+          }
+        : {
+              host: process.env.DB_HOST || "dpg-d66rmc248b3s73curktg-a",
+              user: process.env.DB_USER || "backend_db_user",
+              password: process.env.DB_PASSWORD || "",
+              database: process.env.DB_DATABASE || "diplom_db_46vh",
+              port: process.env.DB_PORT || 5432,
+          },
+);
 
 pool.on("error", (err, client) => {
     console.error("!!! UNEXPECTED ERROR ON IDLE CLIENT !!!", err);
