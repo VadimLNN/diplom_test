98b5f2f4103ec85a824519aefc6c54f8a1652751
VadimLNN <svobodolubsev@inbox.ru>
2026-02-12 20:40:38 +0700

    создание таблиц при включении сервера

--- server/server.js
+++ server/server.js
@@ -3,9 +3,23 @@ const PORT = process.env.PORT || 5000;
 const HOCO_PORT = process.env.HOCO_PORT;
 const hocuspocusServer = require("./realtime/hocuspocus_server");
 
-server.listen(PORT, () => {
-    console.log(`🚀 Server: http://...:${PORT}`);
-    console.log(`📚 Swagger: http://...:${PORT}/api-docs`);
+const fs = require("fs");
+const path = require("path");
+const pool = require("./db");
+
+async function initDb() {
+    const sqlPath = path.join(__dirname, "db.sql");
+    const sql = fs.readFileSync(sqlPath, "utf8");
+
+    await pool.query(sql);
+    console.log("✅ DB initialized");
+}
+
+initDb().then(() => {
+    server.listen(PORT, () => {
+        console.log(`🚀 Server: http://...:${PORT}`);
+        console.log(`📚 Swagger: http://...:${PORT}/api-docs`);
+    });
 });
 
 hocuspocusServer.listen(HOCO_PORT, () => {
